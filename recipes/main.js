137bc502854989d4a0605e0026b2c01bf5afa1a7
iCarrin isaiahcarrin@gmail.com <isaiahcarring@gmail.com>
2024-11-30 18:30:08 -0700

    made up a graph as best I could

--- recipes/main.js
+++ recipes/main.js
@@ -2,44 +2,43 @@ import recipes from './recipes.mjs'
 
 const insert_location = document.querySelector("main");
 
-const rand_rec = recipes[Math.floor(Math.random()* recipes.length)];
+const recipe_to_display = recipes[Math.floor(Math.random()* recipes.length)];
 
-function insert_recipe(rand_rec) {
+function insert_recipe(recipe_to_display) {
   insert_location.innerHTML += 
 	`
 	<div class="recipe_entry">
-		<img class="food_image" src="${rand_rec.image}" alt="">     
-        <div class="content_div">
+		<img class="food_image" src="${recipe_to_display.image}" alt="">     
+		<div class="content_div">
 			<div class="tag_section">
-				${rand_rec.tags.map(tag => `<h4 class="tag">${tag}</h4>`).join('')}
+				${recipe_to_display.tags.map(tag => `<h4 class="tag">${tag}</h4>`).join('')}
 			</div>
-			<h3 class="recipe_name">${rand_rec.name}</h3>
+			<h3 class="recipe_name">${recipe_to_display.name}</h3>
 			<span
 				class="rating"
 				role="img"
-				aria-label="Rating: ${rand_rec.rating} out of 5 stars"
+				aria-label="Rating: ${recipe_to_display.rating} out of 5 stars"
 			>
 				${Array(5).fill('').map((_, i) => //AI wrote this for me when I asked for something else, but I mostly understand it
-				i < Math.floor(rand_rec.rating)
+				i < Math.floor(recipe_to_display.rating)
 				? `<span aria-hidden="true" class="icon-star">⭐</span>`
 				: `<span aria-hidden="true" class="icon-star-empty">☆</span>`
 				).join('')}
 			</span>
-			<p class="description">${rand_rec.description}</p>
+			<p class="description">${recipe_to_display.description}</p>
 		</div>
 	</div>
    `
 };
 
-insert_recipe(rand_rec);
+insert_recipe(recipe_to_display);
 // console.log(recipes);
 
 
 function filterRecipes () {
     const searched_value = document.getElementById("search").value.toLowerCase();
 	// const searched_value = "entree";
-	let sortedRecipes = recipes
-		.filter(recipe => 
+	let sortedRecipes = recipes.filter(recipe => 
 		recipe.name.toLowerCase().includes(searched_value) ||
 		recipe.description.includes(searched_value) ||
 		recipe.tags.some(tag => tag.toLowerCase().includes(searched_value)) ||
@@ -70,6 +69,4 @@ document.querySelector(".search_form").addEventListener('submit', function(event
 	{
 		insert_recipe(toDisplay[i]);
 	};
-
-
 });
